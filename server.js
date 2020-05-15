b51f0cd24fcaed8cb1b51828b82845a3b3fa7c0a
wongon <jejaforce@gmail.com>
2020-05-15 18:53:51 +0900

    nodeList 를 받아오는 새로운 로직 구현

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(port, ()=>{console.log(`server listen ${port}`)});
